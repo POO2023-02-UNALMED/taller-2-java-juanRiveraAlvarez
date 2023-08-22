c71b6f014621ad9500c39e9a53b91908485328c9
juanRiveraAlvarez <juanpariveraalvarez@gmail.com>
2023-08-21 19:51:24 -0700

    Intento1

---
+++ src/main/java/Asiento.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Asiento {
+	
+	String color;
+	int precio;
+	int  registro;
+	
+	void CambiarColor(String color) {
+		this.color = color;
+	}
+
+}
---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,37 @@
+package test;
+
+public class Auto {
+	
+	String modelo;
+	int  precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	int cantidadCreados;
+	
+	int cantidadAsientos() {
+		return this.asientos.length();
+	}
+	
+	String carificarIntegridad() {
+		boolean verificado = false;
+		if(this.motor.registro == this.registro) {
+			for(int  i = 0; i <= asientos.length(); i++) {
+				if(asientos[i] != this.registro) {
+					verificado = true;
+					break;
+				}
+			}
+			if (verificado == false) {
+				return "Auto original"
+			}else {
+				return "Las piezas no son originales";
+			}
+		}else {
+			return "Las piezas no son originales";
+		}
+	}
+	
+
+}
